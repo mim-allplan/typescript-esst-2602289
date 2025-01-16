65e3edf09f4b8343eed7679c3e543da3bd6cf0c4
mim-allplan <mmiani@allplan.com>
2025-01-16 13:29:36 +0000

    Falscher Terminalbereich

---
+++ 04_07/classes.js
@@ -0,0 +1,12 @@
+class Person {
+    name;
+    static field = "Hey!";
+    constructor(name) {
+        this.name = name;
+    }
+}
+export function Demo() {
+    const person = new Person("Ted");
+    console.log(person.name); // output
+}
+Demo();
